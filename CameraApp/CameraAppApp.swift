1139c9ef0544a74b6e9d0983a17f571d6691083c
farz1212 <farzaad1212@gmail.com>
2024-10-24 10:58:01 -0400

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Farzaad Goiporia on 2024-10-24.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
